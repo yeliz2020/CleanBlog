7f5db56a5c04217ba00fbfbc79b5f83c0405a9af
Yeliz İşler <yeliz.isler@gmail.com>
2021-11-11 20:08:57 +0300

    EJS added

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
 app.use(express.static('public'));
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
