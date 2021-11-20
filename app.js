33564b712a3067d17136c7c31f304d490ecda1a7
Yeliz İşler <yeliz.isler@gmail.com>
2021-11-20 22:14:17 +0300

    edit.ejs created, get and put req. added to update posts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const ejs = require('ejs');
 const path = require('path');
@@ -20,10 +21,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 // ROUTES
 app.get('/', async (req, res) => {
-  const posts = await Post.find({});
+  const posts = await Post.find({}).sort('-dateCreated');
   res.render('index', {
     posts,
   });
@@ -49,6 +51,20 @@ app.get('/post', (req, res) => {
 app.post('/posts', async (req, res) => {
   await Post.create(req.body);
   res.redirect('/');
+});
+
+app.get('/posts/edit/:id', async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render('edit', { post });
+});
+
+app.put('/posts/:id', async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title;
+  post.detail = req.body.detail;
+  post.save();
+
+  res.redirect(`/posts/${req.params.id}`);
 });
 
 const port = 3000;
