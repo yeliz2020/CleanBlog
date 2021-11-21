f8b89cfdcaf03567b4e7304e267957e79db23dbf
Yeliz İşler <yeliz.isler@gmail.com>
2021-11-21 21:40:15 +0300

    Controllers folder added

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
-const ejs = require('ejs');
-const path = require('path');
-const Post = require('./models/Post');
+//const ejs = require('ejs');
+//const path = require('path');
+//const Post = require('./models/Post');
+const postController = require('./controllers/postControllers');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -28,53 +30,16 @@ app.use(
 );
 
 // ROUTES
-app.get('/', async (req, res) => {
-  const posts = await Post.find({}).sort('-dateCreated');
-  res.render('index', {
-    posts,
-  });
-});
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.createPost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post,
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-app.get('/post', (req, res) => {
-  res.render('post');
-});
-
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
-
-app.get('/posts/edit/:id', async (req, res) => {
-  const post = await Post.findOne({ _id: req.params.id });
-  res.render('edit', { post });
-});
-
-app.put('/posts/:id', async (req, res) => {
-  const post = await Post.findOne({ _id: req.params.id });
-  post.title = req.body.title;
-  post.detail = req.body.detail;
-  post.save();
-
-  res.redirect(`/posts/${req.params.id}`);
-});
-
-app.delete('/posts/:id', async (req, res) => {
-  await Post.findByIdAndRemove(req.params.id);
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPostPage);
+app.get('/post', pageController.getPostPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
